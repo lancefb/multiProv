cb082722f65daaa7816ee5bb1e0e09ba9db2062d
Lance <lancefb@gmail.com>
2015-04-15 18:12:43 -0700

    combine Bootstrap3 and Semantic UI components for testing

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # multiProv
 
-**mulitProv** is a Meteor test app that
+**mulitProv** is a Meteor test app
 
-1. I made to learn the basics of Meteor
+1. I've made to learn the basics of Meteor
 2. I'm using to experiment with different learning modes
 3. displays multiple proverbs on one page
 
-**mulitProv** uses pagination and the Semantic UI accordion module
+**mulitProv** uses pagination, bootstrap3 popover and accordion module as well as the Semantic UI accordion module
 
 ## To Do
 
--- client/app.js
+++ client/app.js
@@ -1,5 +1,49 @@
 myPagination = new Paginator(ProverbDeck);
 
+Template.PagedPopovers.helpers({
+  proverbs: function() {
+    return myPagination.find({}, {
+      itemsPerPage: 4
+    });
+  },
+  showResetButton: function() {
+    return myPagination.find().currentPage() > 0;
+  }
+});
+
+Template.PagedPopovers.events({
+  'click .reset': function() {
+    return myPagination.reset();
+  }
+});
+
+Template.PopMeOver.rendered = function(){
+  $('[data-toggle="popover"]').popover()
+  };
+
+
+  Template.PagedAccordions.helpers({
+    proverbs: function() {
+      return myPagination.find({}, {
+        itemsPerPage: 4
+      });
+    },
+    showResetButton: function() {
+      return myPagination.find().currentPage() > 0;
+    },
+    rendered: function(){
+    }
+  });
+
+  Template.PagedAccordions.events({
+    'click .reset': function() {
+      return myPagination.reset();
+    }
+  });
+
+
+
+
 Template.PagedProvs.helpers({
   proverbs: function() {
     return myPagination.find({}, {
